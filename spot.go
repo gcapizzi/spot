1ee5c2de9f3cd5ba7211f65edbc625feb95fb026
Giuseppe Capizzi <g.capizzi@gmail.com>
2017-07-14 00:22:40 +0100

    Formatting

--- spot.go
+++ spot.go
@@ -13,9 +13,7 @@ func main() {
 	clientSecret := os.Getenv("SPOT_CLIENT_SECRET")
 
 	authUrl, clientChannel := spotify.Authenticate(clientId, clientSecret)
-
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", authUrl)
-
 	client := <-clientChannel
 
 	userId, err := client.CurrentUserId()
