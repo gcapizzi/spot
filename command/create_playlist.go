6e3c22ede2aaae12a0b3281122f6c8716777652a
Giuseppe Capizzi <g.capizzi@gmail.com>
2017-07-20 23:58:52 +0100

    Extract CreatePlaylistCommand

---
+++ command/create_playlist.go
@@ -0,0 +1,34 @@
+package command
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/gcapizzi/spot/spotify"
+)
+
+type CreatePlaylistCommand struct {
+	client spotify.Client
+}
+
+func NewCreatePlaylistCommand(client spotify.Client) CreatePlaylistCommand {
+	return CreatePlaylistCommand{client: client}
+}
+
+func (c CreatePlaylistCommand) Run() error {
+	playlist, err := c.client.CreatePlaylist("spot")
+	if err != nil {
+		return err
+	}
+
+	tracklistScanner := bufio.NewScanner(os.Stdin)
+	for tracklistScanner.Scan() {
+		trackQuery := tracklistScanner.Text()
+		track, err := c.client.FindTrack(trackQuery)
+		if err == nil {
+			c.client.AddTrackToPlaylist(playlist, track)
+		}
+	}
+
+	return nil
+}
--- spot.go
+++ spot.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/gcapizzi/spot/command"
 	"github.com/gcapizzi/spot/spotify"
 )
 
 func main() {
+	client := authenticate()
+
+	userId, err := client.CurrentUserId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Logged in as ", userId)
+
+	createPlaylistCommand := command.NewCreatePlaylistCommand(client)
+
+	err = createPlaylistCommand.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func authenticate() spotify.Client {
 	clientId := os.Getenv("SPOT_CLIENT_ID")
 	clientSecret := os.Getenv("SPOT_CLIENT_SECRET")
 
@@ -20,23 +37,5 @@ func main() {
 	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", authUrl)
 	client := <-clientChannel
 
-	userId, err := client.CurrentUserId()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Logged in as ", userId)
-
-	playlist, err := client.CreatePlaylist("spot")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tracklistScanner := bufio.NewScanner(os.Stdin)
-	for tracklistScanner.Scan() {
-		trackQuery := tracklistScanner.Text()
-		track, err := client.FindTrack(trackQuery)
-		if err == nil {
-			client.AddTrackToPlaylist(playlist, track)
-		}
-	}
+	return client
 }
