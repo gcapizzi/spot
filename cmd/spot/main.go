f62ba93dc3486b29ba93f31bba8c34fe4784e145
Giuseppe Capizzi <g.capizzi@gmail.com>
2021-01-10 16:21:18 +0100

    Introduce spf13/cobra

--- cmd/spot/main.go
+++ cmd/spot/main.go
@@ -2,32 +2,47 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/gcapizzi/spot/internal/playlist"
 	"github.com/gcapizzi/spot/internal/spotify"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	clientID := os.Getenv("SPOT_CLIENT_ID")
-	clientSecret := os.Getenv("SPOT_CLIENT_SECRET")
+	var client spotify.Client
 
-	authURL, clientChannel, err := spotify.Authenticate(clientID, clientSecret)
-	if err != nil {
-		log.Fatal(err)
+	var rootCmd = &cobra.Command{
+		Use:   "spot",
+		Short: "Spot is the Spotify swiss army knife.",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			clientID := os.Getenv("SPOT_CLIENT_ID")
+			clientSecret := os.Getenv("SPOT_CLIENT_SECRET")
+
+			authURL, clientChannel, err := spotify.Authenticate(clientID, clientSecret)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Navigate to the following URL in your browser:\n\n%s\n\n", authURL)
+
+			client = <-clientChannel
+			fmt.Println("Authenticated successfully.")
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			playlistParser := playlist.NewParser(client)
+
+			err := playlistParser.CreatePlaylistFromText("spot", os.Stdin)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(`Playlist "spot" created.`)
+
+			return nil
+		},
 	}
-	fmt.Printf("Navigate to the following URL in your browser:\n\n%s\n\n", authURL)
 
-	client := <-clientChannel
-	fmt.Println("Authenticated successfully.")
-
-	playlistParser := playlist.NewParser(client)
-
-	err = playlistParser.CreatePlaylistFromText("spot", os.Stdin)
-	if err != nil {
-		log.Fatalf("Error: %s.", err)
-	}
-
-	fmt.Println(`Playlist "spot" created.`)
+	rootCmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/onsi/gomega v1.10.4
+	github.com/spf13/cobra v1.1.1
 	github.com/zmb3/spotify v0.0.0-20201231194903-e2d01d9b8bd2
 )
