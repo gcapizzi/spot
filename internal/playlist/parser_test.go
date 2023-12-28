39dab476cdeed9d5874e47a537df493ab88fc5df
Giuseppe Capizzi <g.capizzi@gmail.com>
2023-12-28 01:47:45 +0100

    Use subtests

--- internal/playlist/parser_test.go
+++ internal/playlist/parser_test.go
@@ -15,64 +15,62 @@ import (
 func TestParser(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	client := NewFakeClient()
-	client.Tracks = map[string]playlist.Track{
-		"one":   {ID: "1"},
-		"two":   {ID: "2"},
-		"three": {ID: "3"},
-	}
+	t.Run("CreatePlaylistFromText", func(t *testing.T) {
+		t.Run("when everything works", func(t *testing.T) {
+			client := NewFakeClient()
+			client.Tracks = map[string]playlist.Track{
+				"one":   {ID: "1"},
+				"two":   {ID: "2"},
+				"three": {ID: "3"},
+			}
+			playlistParser := playlist.NewParser(client)
 
-	playlistParser := playlist.NewParser(client)
-	playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("one\ntwo\nthree"))
+			playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("one\ntwo\nthree"))
 
-	g.Expect(client.Playlists).To(HaveLen(1))
-	g.Expect(client.Playlists["playlist"]).To(Equal([]playlist.Track{
-		{ID: "1"},
-		{ID: "2"},
-		{ID: "3"},
-	}))
-}
+			g.Expect(client.Playlists).To(HaveLen(1))
+			g.Expect(client.Playlists["playlist"]).To(Equal([]playlist.Track{
+				{ID: "1"},
+				{ID: "2"},
+				{ID: "3"},
+			}))
+		})
 
-func TestParserWithEmptyText(t *testing.T) {
-	g := NewGomegaWithT(t)
+		t.Run("when the text is empty", func(t *testing.T) {
+			client := NewFakeClient()
+			playlistParser := playlist.NewParser(client)
 
-	client := NewFakeClient()
+			err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("\n\n\n"))
 
-	playlistParser := playlist.NewParser(client)
-	err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("\n\n\n"))
+			g.Expect(err).To(MatchError(`no tracks found, playlist "playlist" not created`))
+			g.Expect(client.Playlists).To(BeEmpty())
+		})
 
-	g.Expect(err).To(MatchError(`no tracks found, playlist "playlist" not created`))
-	g.Expect(client.Playlists).To(BeEmpty())
-}
+		t.Run("when creating the playlist fails", func(t *testing.T) {
+			client := NewFakeClient()
+			client.Tracks = map[string]playlist.Track{
+				"foo": {ID: "bar"},
+			}
+			client.CreatePlaylistErr = errors.New("create-playlist-error")
 
-func TestParserFailingToCreatePlaylist(t *testing.T) {
-	g := NewGomegaWithT(t)
+			playlistParser := playlist.NewParser(client)
+			err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("foo"))
 
-	client := NewFakeClient()
-	client.Tracks = map[string]playlist.Track{
-		"foo": {ID: "bar"},
-	}
-	client.CreatePlaylistErr = errors.New("create-playlist-error")
+			g.Expect(err).To(MatchError("create-playlist-error"))
+		})
 
-	playlistParser := playlist.NewParser(client)
-	err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("foo"))
+		t.Run("when creating the playlist fails", func(t *testing.T) {
+			client := NewFakeClient()
+			client.Tracks = map[string]playlist.Track{
+				"foo": {ID: "bar"},
+			}
+			client.AddTrackToPlaylistErr = errors.New("add-track-error")
 
-	g.Expect(err).To(MatchError("create-playlist-error"))
-}
+			playlistParser := playlist.NewParser(client)
+			err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("foo"))
 
-func TestParserFailingToAddTrackToPlaylist(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	client := NewFakeClient()
-	client.Tracks = map[string]playlist.Track{
-		"foo": {ID: "bar"},
-	}
-	client.AddTrackToPlaylistErr = errors.New("add-track-error")
-
-	playlistParser := playlist.NewParser(client)
-	err := playlistParser.CreatePlaylistFromText(context.Background(), "playlist", strings.NewReader("foo"))
-
-	g.Expect(err).To(MatchError("add-track-error"))
+			g.Expect(err).To(MatchError("add-track-error"))
+		})
+	})
 }
 
 func NewFakeClient() *FakeClient {
